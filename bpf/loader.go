15e630fe348cbdb34c4d7cc14f541f66b4253de7
wangyd1988 <wang_yudong@inspur.com>
2025-12-15 09:43:53 +0800

    no message

--- bpf/loader.go
+++ bpf/loader.go
@@ -2,4 +2,5 @@
 
 package bpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang bpf bpf_tcp_option_kern.c -- -O2 -g -Wall -Werror -I/usr/include/bpf -I/usr/include/x86_64-linux-gnu
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang bpf bpf_tcp_option_kern.c -- -O2 -g -Wall -Werror -I/usr/include/x86_64-linux-gnu
